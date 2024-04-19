559e248bb9764f225ba67fa86c18568c0431f4be
NaHoJin <ghwls3995@gmail.com>
2024-04-19 10:45:11 +0900

    first commit

---
+++ java_Work/src/ex_240419/todayLunch.java
@@ -0,0 +1,12 @@
+package ex_240419;
+
+public class todayLunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("오늘 점심은 무엇일까요??");
+
+	}
+
+}
